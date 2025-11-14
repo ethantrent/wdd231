55c1e3b8e8af4d92dde11de00fe1dd78f55f316b
Ethan <ethanjotrent@gmail.com>
2025-11-14 11:52:56 -0700

    Refactor main.js to remove duplicate navigation code

--- src/js/main.js
+++ src/js/main.js
@@ -18,36 +18,6 @@ function setParkInfoLinks(data) {
   infoEl.insertAdjacentHTML("afterbegin", html.join(""));
 }
 
-function enableNavigation() {
-  // use a querySelector to get the menu button
-  const menuButton = document.querySelector("#global-nav-toggle");
-  const globalNav = document.querySelector(".global-nav");
-    
-  // when the main menu button is clicked:
-  menuButton.addEventListener("click", (ev) => {
-    let target = ev.target;
-    
-    // check to see if target is the button or something inside the button
-    if (target.tagName !== "BUTTON") {
-      target = target.closest("button");
-    }
-    
-    // toggle the show class on the global-nav
-    globalNav.classList.toggle("show");
-    
-    // check to see if we just opened or closed the menu
-    if (globalNav.classList.contains("show")) {
-      // if we opened it then set the aria-expanded attribute on the button to true
-      target.setAttribute("aria-expanded", "true");
-      target.setAttribute("aria-label", "Close Menu");
-    } else {
-      // if we closed it then set the aria-expanded attribute on the button to false
-      target.setAttribute("aria-expanded", "false");
-      target.setAttribute("aria-label", "Open Menu");
-    }
-  });
-}
-
 async function init() {
   const parkData = await getParkData();
   const parkInfoLinks = getInfoLinks(parkData.images);
@@ -55,7 +25,6 @@ async function init() {
   setHeaderFooter(parkData);
   setParkIntro(parkData);
   setParkInfoLinks(parkInfoLinks);
-  enableNavigation();
 }
 
 init();
